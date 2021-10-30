bee46517e75b2800dce5dad30d32d9fa3e9c063e
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-10-30 18:03:23 -0500

    adds post base empty post page

---
+++ pages/post.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const Post = () => (
+  <>
+    <Head>
+      <title>My Super Awesome Blog! - Post 1</title>
+    </Head>
+    <h1>The title of the post</h1>
+    <p>content...</p>
+  </>
+);
+
+export default Post;
